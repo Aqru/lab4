85ebd1cc183f232de8dfc7d26c5b9dad141e314b
Vladimir Stepanov <vovakirishi@gmail.com>
2016-12-11 14:49:50 +0300

    Fixed red test for rolls

--- src/bowling/game/Bowling.java
+++ src/bowling/game/Bowling.java
@@ -2,15 +2,27 @@ package bowling.game;
 
 public class Bowling {
 	public static final int ROLLCOUNT = 20;
+	public final int EXTROLLCOUNT = 23;
 	
+	private int rolli;
+	
+	private int[] rollsArr;
+	
+	Bowling() {
+		rollsArr = new int[EXTROLLCOUNT];
+		rolli = 0;
+	}
 	
 	public void roll(int pins) {
-		
+		rollsArr[rolli++] = pins;
 	}
 	
 	public int getScore() {
 		
-		return 0;
+		int result = 0;
+		for(int i = 0; i < rolli; i++)
+			result += rollsArr[i];
+		return result;
 	}
 
 }
