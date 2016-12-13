3306c0ea3091c5ecc79e95b06f537497468e960f
Ruslan <rkadriev@gmail.com>
2016-12-13 22:01:04 +0300

    Pass simple strike test.

--- src/com/kadriev/Bowling.java
+++ src/com/kadriev/Bowling.java
@@ -6,6 +6,24 @@ package com.kadriev;
  */
 public class Bowling {
     private int score;
+    private boolean frameStart; // used for strike check.
+    private int pinDown; // used for calc spare.
+
+    private int strikeBonusScore; // used for calc score after strike.
+    //private int spareBonusScore; // used for calc score after spare.
+
+    /***
+     * Checking strike condition.
+     * @param pins current roll pins.
+     * @return true if it's strike or false otherwise.
+     */
+    public boolean strikeCheck(int pins) {
+        if (pins == 10){
+            strikeBonusScore+=2;
+            return true;
+        }
+        return false;
+    }
 
     /***
      * Make a roll.
@@ -13,6 +31,22 @@ public class Bowling {
      */
     public void roll (int pins) {
         score+=pins;
+        if (strikeBonusScore > 0){
+            strikeBonusScore--;
+            score+=pins;
+        }
+//        if (spareBonusScore > 0) {
+//            score += pins;
+//        }
+        if (frameStart) {
+            if (!strikeCheck(pins)) {
+                frameStart = false;
+                pinDown = pins;
+            }
+        }
+        else {
+            frameStart = true;
+        }
     }
 
     /***
@@ -26,5 +60,7 @@ public class Bowling {
 
     public Bowling() {
         score = 0;
+        frameStart = true;
+        pinDown = 0;
     }
 }
