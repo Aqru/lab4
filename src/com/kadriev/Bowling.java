4f3c1a42bb86701984ed495f06301295d6dd93f5
Ruslan <rkadriev@gmail.com>
2016-12-13 19:30:39 +0300

    Pass simple roll test.

--- src/com/kadriev/Bowling.java
+++ src/com/kadriev/Bowling.java
@@ -7,11 +7,24 @@ package com.kadriev;
 public class Bowling {
     private int score;
 
+    /***
+     * Make a roll.
+     * @param pins how many pins was knock.
+     */
     public void roll (int pins) {
-
+        score+=pins;
     }
 
+    /***
+     * Get current score
+     * @return current score
+     */
     public int getScore(){
         return score;
+    }
+
+
+    public Bowling() {
+        score = 0;
     }
 }
--- src/com/kadriev/BowlingTest.java
+++ src/com/kadriev/BowlingTest.java
@@ -1,8 +1,8 @@
 package com.kadriev;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+        import org.junit.Assert;
+        import org.junit.Before;
+        import org.junit.Test;
 
 /**
  * Bowling tests.
